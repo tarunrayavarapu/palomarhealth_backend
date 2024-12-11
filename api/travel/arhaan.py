7871e297277d7a9c56568c8473e63d470bc01cc7
ArhaanM123! <arhaansporty@gmail.com>
2024-12-11 10:36:40 -0800

    changed up format for py file

--- api/travel/arhaan.py
+++ api/travel/arhaan.py
@@ -1,38 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify, g
 from flask_restful import Api, Resource
 
-
 arhaan_api = Blueprint('arhaan_api', __name__, url_prefix='/api')
-
-
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(arhaan_api)
 
+class ArhaanAPI:
+    class _A_Person(Resource):
+        def get(self):
+            return jsonify({
+                "name": "Arhaan Memon",
+                "age": 15,
+                "classes": ["AP CSP", "AP Seminar", "AP BIO", "History", "Math 3b"],
+                "Favorite Sport": {"Basketball": "Football"},
+                "Favorite Countries": {"India", "France", "Switcherland"}
+            })
+    
 
-class StudentAPI:
-   def get_student(name):
-       students = {
-           "Arhaan": {
-               "name": "Arhaan Memon",
-               "age": 15,
-               "classes": ("AP CSP", "AP BIO", "AP SEM, History, Math 3b"),
-               "sports": ("Basketball", "Football"),
-               "Food": ("Pizza", "Pasta", "Apple Pie")
-           },
-       }
-       return students.get(name)
-
-
-   class _Arhaan(Resource):
-       def get(self):
-           # Use the helper method to get Arhaan's details
-           arhaan_details = StudentAPI.get_student("Arhaan")
-           return jsonify(arhaan_details)
-
-
-   # Building REST API endpoints
-   api.add_resource(_Arhaan, '/student/arhaan')
-
-
-# Instantiate the StudentAPI to register the endpoints
-student_api_instance = StudentAPI()
+api.add_resource(ArhaanAPI._A_Person, "/arhaan")
