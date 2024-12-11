23dae8ad8dcd8ef92486c48ae492ca474c9fd637
derekkang1 <derek.j.kang@gmail.com>
2024-12-11 10:21:16 -0800

    derek api added

---
+++ api/travel/derek.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, g
+from flask_restful import Api, Resource
+
+derek_api = Blueprint('derek_api', __name__, url_prefix='/api')
+api = Api(derek_api)
+
+class DerekAPI:
+    class Student(Resource):
+        def get(self):
+            return jsonify({
+                "name": "Derek",
+                "age": 15,
+                "school": "Del Norte High School",
+                "favorite": {
+                    "color": "Blue",
+                    "food": "Pasta"
+                }
+            })
+    
+
+api.add_resource(DerekAPI.Student, "/derek")
