9d52705a0cbbee1bf88dd25fd74d8e61360c9d36
Kiruthic-Selvakumar <kiruthic.selvakumar@gmail.com>
2024-12-11 10:34:26 -0800

    kiruthi api fixes

--- api/travel/kiruthic.py
+++ api/travel/kiruthic.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, g
 from flask_restful import Api, Resource
 
-kiruthic_api = Blueprint('_api', __name__, url_prefix='/api')
+kiruthic_api = Blueprint('kiruthic_api', __name__, url_prefix='/api')
 api = Api(kiruthic_api)
 
 class KiruthicAPI:
