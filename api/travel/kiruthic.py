0902cf550375eea9113cd1b2518c2dcdfcb81a19
Kiruthic-Selvakumar <kiruthic.selvakumar@gmail.com>
2024-12-11 10:17:45 -0800

    student.py, and main.py set up

---
+++ api/travel/kiruthic.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify, g
+from flask_restful import Api, Resource
+
+kiruthic_api = Blueprint('_api', __name__, url_prefix='/api')
+api = Api(kiruthic_api)
+
+class KiruthicAPI:
+    class _A_Person(Resource):
+        def get(self):
+            return jsonify({
+                "name": "Kiruthic Selvakumar",
+                "age": 16,
+                "classes": ["AP CSP", "AP Chemistry", "Honors Humanities", "AP Calculus AB", "AP World History"],
+                "favorite": {
+                    "food": "Pizza",
+                    "color": "Blue",
+                }
+            })
+    
+
+api.add_resource(KiruthicAPI._A_Person, "/kiruthic")
