270896ca79ae8cf55763b9c47beb39a6b9870de1
aadibhat09 <aadibhat09@gmail.com>
2025-02-07 08:46:41 -0800

    \Revert "change food review"
    This reverts commit d7f4425e189f46105ba2f43cbd6dabcb0b5fb25f.

--- api/food_review123.py
+++ api/food_review123.py
@@ -5,7 +5,7 @@ from model.food_review123 import FoodReview123
 
 food_review123_api = Blueprint('food_review123_api', __name__, url_prefix='/api')
 
-api = Api(food_review123_api)
+api = Api(food_review123_api, supports_credentials=True)
 
 class FoodAPI:
     class _CRUD(Resource):
