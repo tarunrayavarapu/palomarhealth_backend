3fcc8212a26b5947f6dca7ee838f28b5dcaced0f
aadibhat09 <aadibhat09@gmail.com>
2025-03-02 11:06:33 -0800

    intertravel backup script for crontab

---
+++ scripts/intertravel_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/ubuntu/intertravel_backend
+
+# Verify the installation and check the Python version
+python --version
+
+# Create a virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required Python packages
+pip install -r requirements.txt
+
+cd /home/ubuntu/intertravel_backend
+./scripts/db_backup.py
